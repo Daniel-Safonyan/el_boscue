b176f7dceb5a983e297e617bbb7319da56caee47
Daniil Safonov <safonov@mail.usf.edu>
2020-09-16 16:14:07 +0300

    Updating

--- src/wordDataClass.kt
+++ src/wordDataClass.kt
@@ -1,7 +1,12 @@
-class Word(){
+class Word(val english: String, val spanish: String, val img: String? = null){
+    fun sayIt() {
+        println("Hola, $english is $spanish in Spanish!")
+    }
+}
 
-    val english: String = ""
-    val spanish: String = ""
-    val img: String = ""
-
+fun main() {
+    val dog = Word("dog", "el perro","src/img/dog.jpg")
+    dog.sayIt()
+    val imgg = dog.img?: "no image"
+    println(imgg)
 }
