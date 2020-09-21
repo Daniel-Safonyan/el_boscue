b6854525c5da1cb06750860f282134a6907c079b
Daniil Safonov <safonov@mail.usf.edu>
2020-09-21 21:12:44 +0300

    Created a irregularVerbsDictionary class!

--- src/wordDataClass.kt
+++ src/wordDataClass.kt
@@ -1,14 +1,20 @@
-data class Word(val english: String, val spanish: String, val img: String? = null){
+data class Word(val english: String, val topic: String, val spanish: String, val img: String? = null){
     fun sayIt() {
         println("Hola, $english is $spanish in Spanish!")
     }
 }
 
-class Dictionary<T> {
+abstract class Dictionary<T>(open val topic: String) {
     val dictionary: MutableList<T> = mutableListOf()
+}
+
+class irregularVerbsDictionary<T>(override val topic: String): Dictionary<T>(topic){
+
 }
 
 fun main() {
     val boat = Word("boat", "el barco")
     boat.sayIt()
+
+    val
 }
